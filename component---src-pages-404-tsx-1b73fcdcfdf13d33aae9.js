a83872996ced8ef4ece7cebe3f55630f12cc35bd
Nagi <nagi@poifuture.com>
2019-08-10 10:22:57 +0800

    Clean js map

--- component---src-pages-404-tsx-1b73fcdcfdf13d33aae9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{326:function(e,n,a){"use strict";a.r(n);var t=a(0),u=a.n(t),c=a(167);n.default=function(){return u.a.createElement("div",null,u.a.createElement(c.a,null),"404 Page")}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-1b73fcdcfdf13d33aae9.js.map
