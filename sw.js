fcb82ec2a7461490f39fc9e9ff92973d8ad7a79f
Nagi <nagi@poifuture.com>
2019-08-26 14:49:26 +0800

    Release 20190826.1

--- sw.js
+++ sw.js
@@ -27,71 +27,80 @@ workbox.clientsClaim();
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "8d938348391be2c1832c48faccfcc5c4"
+    "revision": "61eaef739860f941eb9ea6a6bc7058eb"
   },
   {
     "url": "404/index.html",
-    "revision": "9154292cf736f6b09c5c9befa991dba1"
+    "revision": "a7aefcf6bdd9520455b2743958a7c40e"
   },
   {
     "url": "dev/index.html",
-    "revision": "b538b33a78217d3e7efa3bc9f6aad400"
+    "revision": "2c30749e77934f7a14ceea28e2ac59e8"
   },
   {
     "url": "dumpdata/index.html",
-    "revision": "dbf2ac7d90492f689a575a8df4c356d3"
+    "revision": "5218ebb12259d8651c3d290fd66d44b9"
   },
   {
     "url": "en/index.html",
-    "revision": "ce34944ca5987cec5b1af643d3edd93d"
+    "revision": "1c3b397aca57ef1dc29ace279100fa49"
   },
   {
     "url": "index.html",
-    "revision": "30719662e38ca01b6bfa652eff78421c"
+    "revision": "5f5ad305139133691f57f7cb681d5201"
   },
   {
     "url": "mamechishiki/index.html",
-    "revision": "fdd1d888c45fd14d114b8bc6cd00bd92"
+    "revision": "16cf010aa64ef9e56a1b5a2d6da4eb9d"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9a93d04e9eb737c802d2cf7128eddadf"
+    "revision": "94f0735036d4b5c8898021bd3ea1c490"
   },
   {
     "url": "profile/index.html",
-    "revision": "d23377acd95e761663e1502d7ada2a6b"
+    "revision": "b7afdf5c9f6a721f4391616dc01e70eb"
   },
   {
     "url": "tegami/index.html",
-    "revision": "2f7ab05dee6185a050b0fb728557d3d2"
+    "revision": "193464217b9dcdf54f1f2c278fcb74a3"
   },
   {
     "url": "zh-cn/index.html",
-    "revision": "172b62b15d1df552ca0d3f4d932f9f9a"
+    "revision": "147a68f4ce861aaab19e7e9ff0380705"
   },
   {
     "url": "zh-hans-c2/index.html",
-    "revision": "41f542e75ee0a00474d001b6c0adf95b"
+    "revision": "3ccc7d5da406963ba46debe4ac43061a"
   },
   {
     "url": "zh-hans/index.html",
-    "revision": "7eaf623c258cc11907daf930a8f12d78"
+    "revision": "c91bd55f80d5ee15506db7d11e761e56"
   },
   {
     "url": "zh/index.html",
-    "revision": "4de58a5bea77fa49e36e2fa7d95e9bd1"
+    "revision": "87f0710de156d02e2713d58510b3e178"
   },
   {
     "url": "0-595e543fc8bd72881b28.js"
   },
   {
+    "url": "0-97cb340079186fb6ac6e.js"
+  },
+  {
     "url": "1-6eeb44888772f43a1284.js"
+  },
+  {
+    "url": "18-bf25f2d915209a16939b.js"
   },
   {
     "url": "18-ce45a520f5236b850602.js"
   },
   {
     "url": "app-3453e36e5f307a71ebb2.js"
+  },
+  {
+    "url": "app-9cb3adb9d00edd2e26a8.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-7edd174020620717ab05.js"
@@ -112,10 +121,16 @@ self.__precacheManifest = [
     "url": "component---src-pages-index-tsx-05a52bb45a0a7aa59f67.js"
   },
   {
+    "url": "component---src-pages-index-tsx-1d922e17302bead85ed7.js"
+  },
+  {
     "url": "component---src-pages-index-tsx-70278c1af8ac0a647b82.js"
   },
   {
     "url": "component---src-pages-mamechishiki-tsx-c43315aef3116ef0359a.js"
+  },
+  {
+    "url": "component---src-pages-profile-tsx-2cf13b5eef70d867f924.js"
   },
   {
     "url": "component---src-pages-profile-tsx-7f18393b2bd2969f1d7b.js"
@@ -145,75 +160,110 @@ self.__precacheManifest = [
     "url": "webpack-runtime-76ade09672ed1ee020f5.js"
   },
   {
+    "url": "webpack-runtime-98532ec458d7a84acff3.js"
+  },
+  {
     "url": "webpack-runtime-cc348bf62c22a94f9132.js"
   },
   {
     "url": "chunk-map.json",
-    "revision": "6d649b2717e63b43ffff64949b91df1d"
+    "revision": "9e816083cd333583e842f68e46345e24"
+  },
+  {
+    "url": "locales/zh-hans-c2/CommandsBar.json",
+    "revision": "eddbfef3723246dae16ee0bc3959bbe7"
+  },
+  {
+    "url": "locales/zh-hans-c2/LandingPage.json",
+    "revision": "d51fe8dfa0e507a0eed9579f9af7a7a6"
+  },
+  {
+    "url": "locales/zh-hans/LandingPage.json",
+    "revision": "b6988c07c8294e3ab9be366518eea987"
+  },
+  {
+    "url": "locales/zh/CommandsBar.json",
+    "revision": "6a110a7410f1ad6c3e483311e1af0448"
+  },
+  {
+    "url": "locales/zh/Common.json",
+    "revision": "a08cecaf812868ffc526adc4b558573c"
+  },
+  {
+    "url": "locales/zh/GoiTester.json",
+    "revision": "369fd8fd18c610a11f782d4279db764b"
+  },
+  {
+    "url": "locales/zh/LandingPage.json",
+    "revision": "9cc5214698f3fed771cf09daee7e59c1"
+  },
+  {
+    "url": "locales/zh/WordAdder.json",
+    "revision": "075ca1a0e5c6f2cc304312cafafc4c7e"
   },
   {
     "url": "page-data/404.html/page-data.json",
-    "revision": "405e4023a2a56ee70b0febe916e29dc2"
+    "revision": "becd4071278e4c3157ff582828647b7b"
   },
   {
     "url": "page-data/404/page-data.json",
-    "revision": "f340ed090f540f2f65d0b38465a997d2"
+    "revision": "819551f0f76f838b2c39c0228f20f69f"
   },
   {
     "url": "page-data/dev-404-page/page-data.json",
-    "revision": "80b35278d148323fa089f1821ae0674b"
+    "revision": "05ab2a7c447ec01975cc7a2d68849c86"
   },
   {
     "url": "page-data/dev/page-data.json",
-    "revision": "06d54bb4322726557b6304279bd0b803"
+    "revision": "ff7687fa93b69ea06ac34835995b191d"
   },
   {
     "url": "page-data/dumpdata/page-data.json",
-    "revision": "7ff2d8e22e70d4faf942799983e76ce1"
+    "revision": "544d805907838b1271ad99054be24703"
   },
   {
     "url": "page-data/en/page-data.json",
-    "revision": "cec81613c0e02a8a0df0bcaa42a95835"
+    "revision": "aedc3f62c575ae2fe6595ef1eff8d354"
   },
   {
     "url": "page-data/index/page-data.json",
-    "revision": "9b242c25369ce32fdfc373a476512819"
+    "revision": "7a75a0c2c6799f30fd87c69b5a1ed1bb"
   },
   {
     "url": "page-data/mamechishiki/page-data.json",
-    "revision": "94fd8cc9707395ff2128fd834540ba37"
+    "revision": "587321aa7095bfd39f37f08d5cbb105a"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "a4b8f4a4dc758bfee1b5b586d45179b3"
+    "revision": "878f376c9297ec2ccd38bb8207c3d218"
   },
   {
     "url": "page-data/profile/page-data.json",
-    "revision": "12cd28bea0efbb0211cd3b0654bc92ef"
+    "revision": "62feb69e589a4410718864911afdd9cc"
   },
   {
     "url": "page-data/tegami/page-data.json",
-    "revision": "401c3de9895a22759c7f4484b20a9649"
+    "revision": "dcda8840db13a9b1de33afe198fe1cf2"
   },
   {
     "url": "page-data/zh-cn/page-data.json",
-    "revision": "769d7d08e13b038c2c01e853428ff35c"
+    "revision": "ff7f605b0193688794f07f5d21f253c8"
   },
   {
     "url": "page-data/zh-hans-c2/page-data.json",
-    "revision": "b9693aab4d5263114000e1dfbd61dd76"
+    "revision": "2806e42dd722d8122e7cb6fa9c05c4f3"
   },
   {
     "url": "page-data/zh-hans/page-data.json",
-    "revision": "c19bbded74c5a2811d4c8db3f1b1e9e8"
+    "revision": "54b40225847438dcadda7144daa916f1"
   },
   {
     "url": "page-data/zh/page-data.json",
-    "revision": "13c6abb1dbd7c80ce9a533821a0a5db5"
+    "revision": "e4c962ad67e0298cf81183f30972d029"
   },
   {
     "url": "webpack.stats.json",
-    "revision": "da772f9bd67f41c04cdd7d7aa21c7f09"
+    "revision": "4a6c669caa39a9f0e3b4657383fdf954"
   },
   {
     "url": "manifest.webmanifest",
@@ -243,7 +293,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-3453e36e5f307a71ebb2.js`))) {
+  if (!resources || !(await caches.match(`/app-9cb3adb9d00edd2e26a8.js`))) {
     return await fetch(event.request)
   }
 
